b3aeccae74f8d61a4779f8eeae88cc8a99e7432f
FreeBz <freebz.dev@gmail.com>
2014-02-04 03:04:44 +0900

    st5112에서 작업분

---
+++ src/com/freebz/pocopang/model/AnimalAdapter.java
@@ -0,0 +1,42 @@
+package com.freebz.pocopang.model;
+
+import java.util.ArrayList;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class AnimalAdapter extends BaseAdapter {
+	
+	private ArrayList<Animal> animals = new ArrayList<Animal>();
+
+	@Override
+	public int getCount() {
+		return animals.size();
+	}
+
+	@Override
+	public Object getItem(int index) {
+		return getItem(index);
+	}
+
+	@Override
+	public long getItemId(int index) {
+		return index;
+	}
+
+	@Override
+	public View getView(int index, View view, ViewGroup parent) {
+		if (view == null) {
+			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//			view = inflater(R.layout.animal_list_item, parent, false);
+		}
+		
+		Animal animal = animals.get(index);
+		
+		
+		return view;
+	}
+
+}
