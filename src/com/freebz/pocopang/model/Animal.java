152d3d036a2c0e820090a250479d81b35b91e4e1
FreeBz <freebz.dev@gmail.com>
2014-02-04 03:04:44 +0900

    st5112에서 작업분

---
+++ src/com/freebz/pocopang/model/Animal.java
@@ -0,0 +1,27 @@
+package com.freebz.pocopang.model;
+
+public class Animal {
+
+	private int id;
+	private String name;
+	private int ratio;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRatio() {
+		return ratio;
+	}
+	public void setRatio(int ratio) {
+		this.ratio = ratio;
+	}
+}
